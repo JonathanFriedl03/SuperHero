359dc3ab3861fc1685433f31592d7cde6f2d550a
Jonathan Friedl <jfreetz2@gmail.com>
2020-03-17 21:49:51 -0500

    Finished create index and details methods in the superhero controller

--- SuperHeroes/Controllers/SuperHeroController.cs
+++ SuperHeroes/Controllers/SuperHeroController.cs
@@ -4,38 +4,50 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SuperHeroes.Data;
+using SuperHeroes.Models;
 
 namespace SuperHeroes.Controllers
 {
+    
     public class SuperHeroController : Controller
     {
-
+        public ApplicationDbContext db;//global variable passed in to the below parameter
+        public SuperHeroController(ApplicationDbContext context)
+        {
+           db = context; //this gives full access to database
+        }
         // GET: SuperHero
         public ActionResult Index()
         {
-            return View();
+            var superHeroes = db.SuperHeroes;
+            return View(superHeroes);
         }
 
         // GET: SuperHero/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            SuperHero superHero = db.SuperHeroes.Where(a => a.Id == id).FirstOrDefault();
+            return View(superHero);
         }
 
         // GET: SuperHero/Create
         public ActionResult Create()
         {
-            return View();
+            SuperHero superHero = new SuperHero();
+            return View(superHero);
         }
 
         // POST: SuperHero/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(SuperHero superHero)
         {
             try
             {
                 // TODO: Add insert logic here
+               db.SuperHeroes.Add(superHero);
+                db.SaveChanges();
 
                 return RedirectToAction(nameof(Index));
             }
